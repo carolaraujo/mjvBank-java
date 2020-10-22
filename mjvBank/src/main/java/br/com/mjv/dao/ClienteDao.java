d9c91fd060153938fb170fd7063c2c7829b5c0a0
Caroline Araujo <ca.araujosouza@gmail.com>
2020-10-22 07:35:27 -0300

    subindo o projeto

---
+++ mjvBank/src/main/java/br/com/mjv/dao/ClienteDao.java
@@ -0,0 +1,8 @@
+package br.com.mjv.dao;
+
+import br.com.mjv.model.Cliente;
+
+public interface ClienteDao {
+
+	Cliente buscarPorId(Integer id);
+}
---
+++ mjvBank/src/main/java/br/com/mjv/dao/ClienteDaoImpl.java
@@ -0,0 +1,12 @@
+package br.com.mjv.dao;
+
+import br.com.mjv.model.Cliente;
+
+public class ClienteDaoImpl implements ClienteDao {
+
+	@Override
+	public Cliente buscarPorId(Integer id) {
+		return null;
+	}
+
+}
